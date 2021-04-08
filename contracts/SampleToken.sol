10bf730bf9e46a942b6b7ddecd2c63116a0b5806
miquelcabot <miquel.cabot@gmail.com>
2021-04-08 10:32:00 +0200

    Create SampleToken ERC20 token

---
+++ contracts/SampleToken.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.7.3;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract SampleToken is ERC20 {
+  constructor(uint256 initialSupply) ERC20("Sample Token", "SMP") {
+    _mint(msg.sender, initialSupply);
+  }
+}
--- test/Swap.ts
+++ test/Swap.ts
@@ -6,15 +6,24 @@ const UniswapV2Router02Address: string = '0x7a250d5630B4cF539739dF2C5dAcb4c659F2
 
 describe('Swap contract', () => {
   let Swap: ContractFactory
+  let SampleToken: ContractFactory
   let swap: Contract
+  let sampleToken: Contract
 
   beforeEach('deploy', async () => {
+    SampleToken = await ethers.getContractFactory('SampleToken')
+    sampleToken = await SampleToken.deploy(1_000_000)
+
     Swap = await ethers.getContractFactory('Swap')
     swap = await Swap.deploy(UniswapV2Router02Address)
   })
 
   it('Swap contract is deployed', async () => {
     expect(swap.address).to.not.be.undefined;
+  })
+
+  it('Sample token contract is deployed', async () => {
+    expect(sampleToken.address).to.not.be.undefined;
   })
 
 })
